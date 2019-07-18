58e577544be354abe9c9c180f83467b35a3a8ccc
Padma Pasupathi <padmagitha@gmail.com>
2019-07-18 00:24:18 -0400

    Update Spec.hs

--- test/Test1.hs
+++ test/Test1.hs
@@ -40,7 +40,8 @@ import Test.Hspec
 spec :: Spec
 spec =
     describe "Simplify spec" $
-    specify "simplify scalar one zero" $ do
+    specify "simplify scalar one zero" $ -- Zero Dimension cases
+     do
         x `shouldBe` x
         simplify (const 1 / x) `shouldBe` simplify (x ^ (-1))
         simplify (x + x) `shouldBe` const 2 *. x
@@ -121,6 +122,10 @@ spec =
                 ((x * ((x + (const (-1.0) *. y)) ^ (-1))) +
                  (y * ((x + (const (-1.0) *. y)) ^ (-1))) --FIXME
                  )
+        simplify (x * x) `shouldBe` simplify (x ^ 2)
+        simplify (x ^ 0) `shouldBe` const 1.0
+        simplify (x ^ 1) `shouldBe` simplify x
+--        simplify (const 3*.x)/(const 2*.x) `shouldBe` (const 1 *.x) --FIXME
 --DOT PRODUCT
         simplify (x <.> y) `shouldBe` simplify (x * y)
         simplify (x <.> const 1) `shouldBe` simplify x
@@ -135,3 +140,208 @@ spec =
         simplify ((x <.> (x + y)) ^ 2) `shouldBe`
             simplify
                 (((x ^ 2) * (y ^ 2)) + (const 2.0 *. (y * (x ^ 3))) + (x ^ 4))
+       -- simplify (x+x) <.> y `shouldBe` simplify (const 2.0 *.(x*y)) --(2.0*.(x*y))  But error while testing it FIXME
+        -- simplify (x*.x) <.> x `shouldBe` simplify x^3 -- Error while testing FIXME
+--One Dimension TC
+        simplify x1 `shouldBe` simplify x1
+        simplify (x1 * x1) `shouldBe` simplify (x1 ^ 2)
+        simplify (x1 + x1) `shouldBe` simplify (const 2.0 *. x1)
+--        simplify (x1 - x1) `shouldBe` const 0 -- FIXME error for with n without const 0, 0.0, Zero,
+--        simplify (x1 / x1) `shouldBe` 1 --FIXME
+        simplify (x1 + y1) `shouldBe` simplify ((x1 + y1))
+        simplify (x1 - y1) `shouldBe` simplify (x1 + (const (-1.0) *. y1))
+        simplify (x1 * y1) `shouldBe` simplify (x1 * y1)
+        simplify (x1 / y1) `shouldBe` simplify (x1 * (y1) ^ (-1))
+        simplify ((x1 + y1) ^ 2) `shouldBe`
+            simplify ((const 2.0 *. (x1 * y1)) + ((x1) ^ 2) + ((y1) ^ 2))
+        simplify ((x1 - y1) ^ 2) `shouldBe`
+            simplify ((const (-2.0) *. (x1 * y1)) + ((x1) ^ 2) + ((y1) ^ 2))
+        simplify ((x1 * y1) ^ 2) `shouldBe` simplify (((x1) ^ 2) * ((y1) ^ 2))
+        simplify ((x1 / y1) ^ 2) `shouldBe`
+            simplify (((y1) ^ (-2)) * ((x1) ^ 2))
+        simplify ((x1 <.> y1) ^ 2) `shouldBe` simplify (((x1 <.> y1)) ^ 2)
+        simplify ((const 2 *. x1) ^ 2) `shouldBe`
+            simplify (const 4.0 *. ((x1) ^ 2))
+        simplify ((const 3 *. x1) / (const 2 *. y1)) `shouldBe`
+            simplify (const 3.0 *. (x1 * (((const 2.0 *. y1)) ^ (-1)))) --FIXME
+--        simplify (x1^0) `shouldBe` simplify const 1.0 --FIXME
+        simplify (x1 ^ 1) `shouldBe` simplify (x1)
+        simplify (const 1.0 *. x1) `shouldBe` simplify (x1)
+--        simplify (const 1/x1) `shouldBe` simplify (x1^-1) --FIXME
+--        simplify (x1 / x1) `shouldBe` const 1 --FIXME
+        simplify ((x1 - y1) * (x1 + y1)) `shouldBe`
+            simplify
+                ((const (-1.0) *. ((y1) ^ 2)) + ((x1) ^ 2))
+--        simplify ((x1+y1)/(x1-y1)) `shouldBe` simplify ((x1*(((x1+(const (-1.0)*.y1)))^(-1)))+(y1*(((x1+(const (-1.0)*.y1)))^(-1))))
+--  simplify (x ^ x) `shouldBe` simplify x --Not possible
+
+
+---- All Zero cases to One Dimension cases
+        x1 `shouldBe` x1
+        simplify (x1 + x1) `shouldBe` const 2 *. x1
+        simplify (x1 * x1) `shouldBe` simplify (x1 ^ 2)
+        simplify (x1 + y1) `shouldBe` simplify (x1 + y1)
+        simplify (x1 - y1) `shouldBe` simplify (x1 - y1)
+        simplify (x1 * y1) `shouldBe` simplify (x1 * y1)
+        simplify (x1 / y1) `shouldBe` simplify (x1 * y1 ^ (-1))
+        simplify ((x1 + y1) * (x1 + y1)) `shouldBe` simplify ((const 2.0 *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2))
+        simplify ((x1 + y1) ^ 2) `shouldBe` simplify ((const 2.0 *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2))
+        prettify (simplify ((x1 - y1) ^ 2)) `shouldBe` prettify (simplify ((const (-2.0) *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2)))
+        simplify ((x1 - y1) * (x1 - y1)) `shouldBe` simplify ((const (-2.0) *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2))
+        simplify ((x1 * y1) ^ 2) `shouldBe` simplify ((x1 ^ 2) * (y1 ^ 2))
+        simplify ((x1 * y1) * (x1 * y1)) `shouldBe` simplify ((x1 ^ 2) * (y1 ^ 2))
+        simplify ((x1 / y1) * (x1 / y1)) `shouldBe` simplify ((y1 ^ (-2)) * (x1 ^ 2))
+        simplify ((x1 / y1) ^ 2) `shouldBe` simplify ((y1 ^ (-2)) * (x1 ^ 2))
+--        simplify ((const 1 / x1) * (const 1 / x1)) `shouldBe` simplify (x1 ^ (-2)) --FIXME
+--        simplify ((const 1 / x1) ^ 2) `shouldBe` simplify (x1 ^ (-2)) --FIXME
+        simplify (x1 * x1) `shouldBe` simplify (x1 ^ 2)
+        simplify ((x1 * x1) * x1) `shouldBe` simplify (x1 ^ 3)
+        simplify (x1 * x1 / x1) `shouldBe` simplify x1
+        simplify (x1 / x1 / x1) `shouldBe` simplify (x1 ^ (-1))
+        simplify (x1 / x1 / y1) `shouldBe` simplify (y1 ^ (-1))
+        simplify ((x1 * y1) ^ 3) `shouldBe` simplify ((x1 ^ 3) * (y1 ^ 3))
+        simplify (((x1 * y1) ^ 3) * x1 / y1) `shouldBe` simplify ((y1 ^ 2) * (x1 ^ 4))
+        simplify (((x1 * y1) ^ 3) * x1 + y1) `shouldBe` simplify (y1 + ((y1 ^ 3) * (x1 ^ 4)))
+        simplify (((x1 * y1) ^ 3) * (x1 + y1)) `shouldBe` simplify (((y1 ^ 3) * (x1 ^ 4)) + ((x1 ^ 3) * (y1 ^ 4)))
+        simplify (x1 / (x1 ^ 3)) `shouldBe` simplify (x1 ^ (-2))
+        simplify ((x1 ^ 2) ^ 2) `shouldBe` simplify (x1 ^ 4)
+        simplify ((x1 + y1) * (x1 + y1)) `shouldBe` simplify ((const 2.0 *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2))
+        simplify  (((x1 + y1) * (x1 + y1)) * (x1 + y1)) `shouldBe` simplify ((const 3.0 *. (y1 * (x1 ^ 2))) + ((const 3.0) *. (x1 * (y1 ^ 2))) + (x1 ^ 3) + (y1 ^ 3))
+        simplify ((x1 - y1) ^ 2) `shouldBe` simplify ((const 2.0 *. (x1 * (negate (y1)))) + (x1 ^ 2) + ((negate (y1)) ^ 2))
+        simplify (((x1 * y1) ^ 3) * (x1 + y1) ^ 2) `shouldBe` simplify (((y1 ^ 3) * (x1 ^ 5)) + ((x1 ^ 3) * (y1 ^ 5)) + (const 2.0 *. ((x1 ^ 4) * (y1 ^ 4))))
+        simplify ((x1 + y1) ^ 2) `shouldBe` simplify ((const 2.0 *. (x1 * y1)) + (x1 ^ 2) + (y1 ^ 2))
+        simplify (x1 / (x1 ^ 2)) `shouldBe`  simplify (x1 ^ (-1))
+        simplify (x1 / (x1 * x1)) `shouldBe` simplify (x1 ^ (-1))
+        simplify (x1 / x1 * x1) `shouldBe` simplify x1
+        simplify (x1 / (x1 ^ 3)) `shouldBe` simplify (x1 ^ (-2))
+        simplify ((x1 * x1) / y1) `shouldBe` simplify ((y1 ^ (-1)) * (x1 ^ 2))
+        simplify ((x1 ^ 2) ^ 3) `shouldBe` simplify (x1 ^ 6)
+        simplify (((x1 ^ 3) ^ 3) ^ 2) `shouldBe` simplify (x1 ^ 18)
+        simplify (y1 * (y1 ^ 2) ^ 2) `shouldBe` simplify (y1 ^ 5)
+        simplify (x1 / ((y1 ^ 2) ^ 3)) `shouldBe` simplify (x1 * y1 ^ (-6))
+        simplify (((x1 * y1) ^ 2) / x1) `shouldBe` simplify (x1 * y1 ^ 2)
+        prettify (simplify (((x1 + y1) ^ 2) * ((x1 - y1) ^ 2))) `shouldBe` prettify (simplify ((const (-2.0) *. ((x1 ^ 2) * (y1 ^ 2))) + (x1 ^ 4) + (y1 ^ 4)))
+        simplify (((x1 + y1) ^ 2) + ((x1 - y1) ^ 2)) `shouldBe` simplify ((const 2.0 *. (x1 ^ 2)) + (const 2.0 *. (y1 ^ 2)))
+        simplify (((x1 + y1) ^ 2) - ((x1 - y1) ^ 2)) `shouldBe` simplify (const 4.0 *. (x1 * y1))
+        simplify ((x1 + y1) * (x1 - y1)) `shouldBe` simplify ((const (-1.0) *. (y1 ^ 2)) + (x1 ^ 2))
+        simplify ((x1 + y1) + (x1 - y1)) `shouldBe` simplify (const 2.0 *. x1)
+        simplify ((x1 + y1) - (x1 - y1)) `shouldBe`  simplify (const 2.0 *. y1)
+        simplify ((x1 + y1) / (x1 - y1)) `shouldBe` simplify ((x1 * ((x1 + (const (-1.0) *. y1)) ^ (-1))) + (y1 * ((x1 + (const (-1.0) *. y1)) ^ (-1))) )
+        simplify (x1 * x1) `shouldBe` simplify (x1 ^ 2)
+        --simplify (x1 ^ 0) `shouldBe`  const 1.0 -- FIXME
+        simplify (x1 ^ 1) `shouldBe` simplify x1
+--       --        simplify (const 3*.x1)/(const 2*.x1) `shouldBe` (const 1 *.x1) --FIx1ME
+--       --DOT PRODUCT --FIXME Dot product doesnt work in one dimension
+--        simplify  (x1 <.> y1) `shouldBe` simplify (x1 * y1) --FIXME
+--        simplify (x1 <.> const 1) `shouldBe` simplify x1
+--        simplify (x1 <.> const (-1)) `shouldBe` simplify (const (-1) * x1) --FIXME
+--        simplify (x1 <.> const 0) `shouldBe` const 0
+--        simplify (x1 <.> x1) `shouldBe` simplify x1 ^ 2 --FIXME
+--        simplify (x1 <.> x1 * y1) `shouldBe` simplify (y1 * (x1 ^ 2))
+--        simplify (x1 <.> x1 + x1) `shouldBe` simplify (x1 + (x1 ^ 2)) --FIXME
+--        simplify (x1 <.> (x1 + x1)) `shouldBe` simplify (const 2.0 *. (x1 ^ 2)) --FIXME
+--        simplify (x1 <.> (const 1 / x1)) `shouldBe` const 1 --FIXME
+--        simplify (x1 <.> (y1 / x1)) `shouldBe` simplify y1 --FIXME
+--        simplify ((x1 <.> (x1 + y1)) ^ 2) `shouldBe` simplify (((x1 ^ 2) * (y1 ^ 2)) + (const 2.0 *. (y1 * (x1 ^ 3))) + (x1 ^ 4)) --FIXME
+
+--Two dimension cases
+
+        x2 `shouldBe` x2
+--        simplify (const 1 / x2) `shouldBe` simplify (x2 ^ (-1))
+        simplify (x2 + x2) `shouldBe` const 2 *. x2
+--        simplify (x2 - x2) `shouldBe` const 0
+        simplify (x2 * x2) `shouldBe` simplify (x2 ^ 2)
+--        simplify (x2 / x2) `shouldBe` const 1
+        simplify (x2 + y2) `shouldBe` simplify (x2 + y2)
+        simplify (x2 - y2) `shouldBe` simplify (x2 - y2)
+        simplify (x2 * y2) `shouldBe` simplify (x2 * y2)
+        simplify (x2 / y2) `shouldBe` simplify (x2 * y2 ^ (-1))
+        simplify ((x2 + y2) * (x2 + y2)) `shouldBe`
+            simplify ((const 2.0 *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2))
+        simplify ((x2 + y2) ^ 2) `shouldBe`
+            simplify ((const 2.0 *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2))
+        prettify (simplify ((x2 - y2) ^ 2)) `shouldBe`
+            prettify (simplify ((const (-2.0) *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2)))
+        simplify ((x2 - y2) * (x2 - y2)) `shouldBe`
+            simplify ((const (-2.0) *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2))
+        simplify ((x2 * y2) ^ 2) `shouldBe` simplify ((x2 ^ 2) * (y2 ^ 2))
+        simplify ((x2 * y2) * (x2 * y2)) `shouldBe` simplify ((x2 ^ 2) * (y2 ^ 2))
+        simplify ((x2 / y2) * (x2 / y2)) `shouldBe` simplify ((y2 ^ (-2)) * (x2 ^ 2))
+        simplify ((x2 / y2) ^ 2) `shouldBe` simplify ((y2 ^ (-2)) * (x2 ^ 2))
+--        simplify ((const 1 / x2) * (const 1 / x2)) `shouldBe` simplify (x2 ^ (-2))
+--        simplify ((const 1 / x2) ^ 2) `shouldBe` simplify (x2 ^ (-2))
+        simplify (x2 * x2) `shouldBe` simplify (x2 ^ 2)
+        simplify ((x2 * x2) * x2) `shouldBe` simplify (x2 ^ 3)
+--        simplify (const 1 / x2) `shouldBe` simplify (x2 ^ (-1))
+        simplify (x2 * x2 / x2) `shouldBe` simplify x2
+--        simplify (x2 / x2) `shouldBe` const 1
+        simplify (x2 / x2 / x2) `shouldBe` simplify (x2 ^ (-1)) --Ex2pected output
+        simplify (x2 / x2 / y2) `shouldBe` simplify (y2 ^ (-1)) --((x2*(x2^-1))*(y2^-1))
+        simplify ((x2 * y2) ^ 3) `shouldBe` simplify ((x2 ^ 3) * (y2 ^ 3))
+        simplify (((x2 * y2) ^ 3) * x2 / y2) `shouldBe` simplify ((y2 ^ 2) * (x2 ^ 4))
+        simplify (((x2 * y2) ^ 3) * x2 + y2) `shouldBe`
+            simplify (y2 + ((y2 ^ 3) * (x2 ^ 4))) --Without Paranthesis
+        simplify (((x2 * y2) ^ 3) * (x2 + y2)) `shouldBe`
+            simplify (((y2 ^ 3) * (x2 ^ 4)) + ((x2 ^ 3) * (y2 ^ 4))) -- With paranthesis
+        simplify (x2 / (x2 ^ 3)) `shouldBe` simplify (x2 ^ (-2)) --FIx2ED
+        simplify ((x2 ^ 2) ^ 2) `shouldBe` simplify (x2 ^ 4)
+        simplify ((x2 + y2) * (x2 + y2)) `shouldBe`
+            simplify ((const 2.0 *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2))
+        simplify (((x2 + y2) * (x2 + y2)) * (x2 + y2)) `shouldBe`
+            simplify
+                ((const 3.0 *. (y2 * (x2 ^ 2))) + ((const 3.0) *. (x2 * (y2 ^ 2))) +
+                 (x2 ^ 3) +
+                 (y2 ^ 3))
+--        simplify ((x2 - y2) ^ 2) `shouldBe` simplify (   (const 2.0 *. (x2 * (negate (y2)))) + (x2 ^ 2) + ((negate (y2)) ^ 2))
+        simplify (((x2 * y2) ^ 3) * (x2 + y2) ^ 2) `shouldBe`
+            simplify
+                (((y2 ^ 3) * (x2 ^ 5)) + ((x2 ^ 3) * (y2 ^ 5)) +
+                 (const 2.0 *. ((x2 ^ 4) * (y2 ^ 4))))
+        simplify ((x2 + y2) ^ 2) `shouldBe`
+            simplify ((const 2.0 *. (x2 * y2)) + (x2 ^ 2) + (y2 ^ 2))
+        simplify (x2 / (x2 ^ 2)) `shouldBe` simplify (x2 ^ (-1))
+        simplify (x2 / (x2 * x2)) `shouldBe` simplify (x2 ^ (-1)) --with paranthesis
+        simplify (x2 / x2 * x2) `shouldBe` simplify x2 -- without paranthesis
+        simplify (x2 / (x2 ^ 3)) `shouldBe` simplify (x2 ^ (-2)) -- FIx2ED
+        simplify ((x2 * x2) / y2) `shouldBe` simplify ((y2 ^ (-1)) * (x2 ^ 2))
+        simplify ((x2 ^ 2) ^ 3) `shouldBe` simplify (x2 ^ 6)
+        simplify (((x2 ^ 3) ^ 3) ^ 2) `shouldBe` simplify (x2 ^ 18)
+        simplify (y2 * (y2 ^ 2) ^ 2) `shouldBe` simplify (y2 ^ 5)
+        simplify (x2 / ((y2 ^ 2) ^ 3)) `shouldBe` simplify (x2 * y2 ^ (-6))
+        simplify (((x2 * y2) ^ 2) / x2) `shouldBe` simplify (x2 * y2 ^ 2)
+        prettify (simplify (((x2 + y2) ^ 2) * ((x2 - y2) ^ 2))) `shouldBe`
+            prettify
+                (simplify
+                     ((const (-2.0) *. ((x2 ^ 2) * (y2 ^ 2))) + (x2 ^ 4) + (y2 ^ 4)))
+        simplify (((x2 + y2) ^ 2) + ((x2 - y2) ^ 2)) `shouldBe`
+            simplify ((const 2.0 *. (x2 ^ 2)) + (const 2.0 *. (y2 ^ 2)))
+        simplify (((x2 + y2) ^ 2) - ((x2 - y2) ^ 2)) `shouldBe`
+            simplify (const 4.0 *. (x2 * y2))
+        simplify ((x2 + y2) * (x2 - y2)) `shouldBe`
+            simplify ((const (-1.0) *. (y2 ^ 2)) + (x2 ^ 2))
+        simplify ((x2 + y2) + (x2 - y2)) `shouldBe` simplify (const 2.0 *. x2)
+        simplify ((x2 + y2) - (x2 - y2)) `shouldBe` simplify (const 2.0 *. y2)
+        simplify ((x2 + y2) / (x2 - y2)) `shouldBe`
+            simplify
+                ((x2 * ((x2 + (const (-1.0) *. y2)) ^ (-1))) +
+                 (y2 * ((x2 + (const (-1.0) *. y2)) ^ (-1))) --FIx2ME
+                 )
+        simplify (x2 * x2) `shouldBe` simplify (x2 ^ 2)
+--        simplify (x2 ^ 0) `shouldBe` const 1.0
+        simplify (x2 ^ 1) `shouldBe` simplify x2
+--        simplify (const 3*.x2)/(const 2*.x2) `shouldBe` (const 1 *.x2) --FIx2ME
+
+----DOT PRODUCT
+--        simplify (x2 <.> y2) `shouldBe` simplify (x2 * y2)
+--        simplify (x2 <.> const 1) `shouldBe` simplify x2
+--        simplify (x2 <.> const (-1)) `shouldBe` simplify (const (-1) * x2)
+--        simplify (x2 <.> const 0) `shouldBe` const 0
+--        simplify (x2 <.> x2) `shouldBe` simplify x2 ^ 2
+--        simplify (x2 <.> x2 * y2) `shouldBe` simplify (y2 * (x2 ^ 2))
+--        simplify (x2 <.> x2 + x2) `shouldBe` simplify (x2 + (x2 ^ 2))
+--        simplify (x2 <.> (x2 + x2)) `shouldBe` simplify (const 2.0 *. (x2 ^ 2))
+----        simplify (x2 <.> (const 1 / x2)) `shouldBe` const 1
+--        simplify (x2 <.> (y2 / x2)) `shouldBe` simplify y2
+--        simplify ((x2 <.> (x2 + y2)) ^ 2) `shouldBe`
+--            simplify
+--                (((x2 ^ 2) * (y2 ^ 2)) + (const 2.0 *. (y2 * (x2 ^ 3))) + (x2 ^ 4))
