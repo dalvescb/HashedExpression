326784a46b02ae957d1379c0e9b5fa42e31258ee
ghhabib2 <ghhabib2@gmail.com>
2019-07-29 11:37:57 -0400

    Adding the primary information related to hashedError

---
+++ src/HashedError.hs
@@ -0,0 +1,34 @@
+{-|
+Module      : HashedError
+Description : Responsible for calculating the calculation error amount of expressions based on Interval Analysis.
+Maintainer  : ghaffh1@mcmaster.ca
+
+
+The main idea behind implementation of HaheshError is to track down the amount of error generated based on different
+mathematical operations. For this purpose we are going to use the simple idea of Interval Analyis.
+-}
+module HashedError where
+
+import Data.Array
+import Data.Complex
+import qualified Data.IntMap.Strict as IM
+import Data.Map (Map, fromList)
+import qualified Data.Map as Map
+import Debug.Trace (traceId, traceShowId)
+
+import HashedExpression
+    ( C
+    , ET(..)
+    , Expression(..)
+    , ExpressionMap
+    , Node(..)
+    , One
+    , R
+    , Three
+    , Two
+    , Zero
+    )
+import HashedNode
+import HashedPrettify (prettify, showExp)
+import HashedUtils
+
