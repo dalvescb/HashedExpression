6ef5955d0335c079c425cad752b6db5bc590d713
Curtis D'Alves <dalvescb@ArchMachine.localdomain>
2019-10-09 00:07:45 -0400

    cleaning up HashedToIpopt,added memcpy

--- src/HashedToIpopt.hs
+++ src/HashedToIpopt.hs
@@ -19,6 +19,7 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 import Data.IntMap (IntMap)
+import qualified Data.Map as Map
 import qualified Data.IntMap as I
 import Data.List as L
 
@@ -67,6 +68,7 @@ debug = (flip  trace)
 idNumVars = "n"
 idINPUTSIZE = "INPUTSIZE"
 idMEMSIZE   = "MEMSIZE"
+idCONSTRAINTSIZE = "CONSTRAINTSIZE"
 idNumConstraints = "m"
 idIndex = "ind"
 idLowerBoundsX = "x_L"
@@ -88,6 +90,7 @@ idEvalHess    = "eval_h"
 idSharedData  = "sdata"
 idNELEJAC = "NELE_JAC"
 idNELEHESS = "NELE_HESS"
+idPtr = "ptr"
 
 typeIdIndex = "Index"
 typeIdNumber = "Number"
@@ -95,6 +98,10 @@ typeIdSharedData = "SharedData"
 
 indent str = "  " ++ str
 padding = map T.pack ["",""]
+
+-- TODO move me to HashedToC???
+memcpyCode (destPtr,destIdx) (srcPtr,srcIdx) (typeId,size) =
+  "memcpy(&"++destPtr++"["++show destIdx++"],&"++srcPtr++"["++show srcIdx++"],sizeof("++typeId++")*"++show size++");"
 
 -- | Templates
 --
@@ -107,71 +114,69 @@ mainTemplate valMaps (Problem vars objId exprMap memMap mBoxConstraints mScalarC
      -- code for evaluating objective function
      objEvalCode = generateEvaluatingCodes memMap (exprMap,[objId])
      -- ptr offset (to be used in c code) for each partial derivative and their sizes
-     objDiffOffsetsSize = map (\i -> (memOffset memMap i LookupR, product $ retrieveShape i exprMap)) $
+     objDiffOffsetsSize = map (\idx -> (memOffset memMap idx LookupR, product $ retrieveShape idx exprMap)) $
                           map partialDerivativeId vars
      -- code for evaluating ALL partial derivatives of objective function
      objDiffEvalCode = generateEvaluatingCodes memMap (exprMap,map partialDerivativeId vars)
-     -- variables with thier dimensions/sizes
-     varsWithShapes :: [(String, Shape)]
-     varsWithShapes = map (\v -> (varName v, retrieveShape (nodeId v) exprMap)) vars
-     varsWithSizes :: [(String, Int)]
-     varsWithSizes = map (\(vN,vS) -> (vN,product vS)) varsWithShapes
+     -- variables with their dimensions/sizes
+     varsWithSizes :: [(Variable, Int)]
+     varsWithSizes = map (\v -> (v, product $ retrieveShape (nodeId v) exprMap)) vars
+     -- vairable offsets and sizes
+     varsOffsetsSizes :: [(Int,Int)]
+     varsOffsetsSizes = map (\(v,sz) -> (memOffset memMap (nodeId v) LookupR,sz)) varsWithSizes
      -- total number of primal vars (size of idVarX)
      totalNumVars = sum $ map snd varsWithSizes
+     -- create code to memcpy x to SharedData
+     xPtrOffsetPairs = zip varsOffsetsSizes (scanl (+) 0 $ map snd varsOffsetsSizes)
+     x2PtrMemCpyCode = ["/* copy x to SharedData */"] ++
+                       map (\((oOff,oSz),i) -> memcpyCode (idPtr,oOff) (idVarX,i) (typeIdNumber,oSz))
+                       xPtrOffsetPairs
      -- total memory size of SharedData (includes primals and all other nodes)
      memSize = totalDoubles memMap
-     -- size of jacbian and hessian
-     nele_jac = 0 -- TODO define nele_jac
+     -- size of known nonzeros in jacbian/hessian
+     nele_jac = totalNumVars * constraintSize -- TODO define nele_jac
      nele_hess = 0  -- TODO is nele_hess correct?
-     -- collect partials scalar constraints
+     -- number of constraints
+     constraintSize = length scalarConsIDs
+     -- collect all scalar constraints
+     scalarConsIDs = case mScalarConstraints of
+                       Just xs -> map constraintValueId xs
+                       Nothing -> []
+     scalarConsOffsetSizes = map (\i -> (memOffset memMap i LookupR,
+                                             product $ retrieveShape i exprMap)) $ scalarConsIDs
+     scalarConsEvalCode = generateEvaluatingCodes memMap (exprMap,scalarConsIDs)
+     -- collect partials of scalar constraints
      partialScalarConsIDs = case mScalarConstraints of
                               Just xs -> concatMap constraintPartialDerivatives xs
                               Nothing -> []
      paritalScalarOffsetsSizes = map (\i -> (memOffset memMap i LookupR,
                                              product $ retrieveShape i exprMap)) $ partialScalarConsIDs
      partialScalarConsEvalCode = generateEvaluatingCodes memMap (exprMap,partialScalarConsIDs)
-  in
-  includesTemplate
-  ++ padding
-  ++ typedefTemplate
-  ++ definesTemplate totalNumVars nele_jac nele_hess memSize
-  ++ padding
-  ++ evalfTemplate objOffset objEvalCode
-  ++ padding
-  ++ evalGradfTemplate objDiffOffsetsSize objDiffEvalCode
-  ++ padding
-  ++ evalhTemplate
-  ++ padding
-  ++ evalgTemplate
-  ++ padding
-  ++ evalJacTemplate paritalScalarOffsetsSizes partialScalarConsEvalCode
-  ++ padding
-  ++ map T.pack
-     ["int main()"
-     ,"{"]
-  ++ varDeclareTemplate
-  ++ padding
-  ++ allocateTemplate objOffset
-  -- ++ initializeTemplate TODO include initializeTemplate in main template
-  ++ padding
-  ++ createIpoptTemplate nele_jac nele_hess
-  ++ padding
-  ++ freeBoundsTemplate
-  ++ padding
-  ++ addOptionsTemplate
-  ++ padding
-  ++ ipoptSolveTemplate
-  ++ padding
-  -- ++ printSolutionTemplate TODO include printSolutionTemplate
-  ++ padding
-  ++ freeSolutionTemplate
-  ++ map T.pack
-     ["  return 0;"
-     ,"}"]
+  in concat $ intersperse padding [
+     includesTemplate
+    ,typedefTemplate
+    ,definesTemplate totalNumVars nele_jac nele_hess memSize constraintSize
+    ,evalfTemplate varsOffsetsSizes objOffset objEvalCode x2PtrMemCpyCode
+    ,evalGradfTemplate objDiffOffsetsSize objDiffEvalCode x2PtrMemCpyCode
+    ,evalhTemplate
+    ,evalgTemplate scalarConsIDs scalarConsOffsetSizes scalarConsEvalCode x2PtrMemCpyCode
+    ,evalJacTemplate paritalScalarOffsetsSizes partialScalarConsEvalCode x2PtrMemCpyCode
+    ,map T.pack ["int main()","{"]
+    ,varDeclareTemplate
+    ,allocateTemplate objOffset
+    ,initializeTemplate valMaps memMap exprMap
+    ,createIpoptTemplate nele_jac nele_hess
+    ,freeBoundsTemplate
+    ,addOptionsTemplate
+    ,ipoptSolveTemplate
+    -- ,printSolutionTemplate TODO include printSolutionTemplate
+    ,freeSolutionTemplate
+    ,map T.pack ["  return 0;","}"]
+    ]
 
 includesTemplate :: [T.Text]
 includesTemplate = map T.pack
-  ["#include \"IpStdCInterface.h\""
+ ["#include \"IpStdCInterface.h\""
  ,"#include <stdlib.h>"
  ,"#include <assert.h>"
  ,"#include <stdio.h>"
@@ -183,12 +188,13 @@ typedefTemplate = map T.pack
   ["struct _"++typeIdSharedData++" {"++typeIdNumber++"* data; int data_size; };"
   ,"typedef struct _"++typeIdSharedData++"* "++typeIdSharedData++";"]
 
-definesTemplate :: Int -> Int -> Int -> Int -> [T.Text]
-definesTemplate inputSize neleJac neleHess memSize = map T.pack
+definesTemplate :: Int -> Int -> Int -> Int -> Int -> [T.Text]
+definesTemplate inputSize neleJac neleHess memSize constraintSize = map T.pack
   ["#define "++idINPUTSIZE ++ "  " ++ show inputSize
   ,"#define "++idNELEJAC ++ "  " ++ show neleJac
   ,"#define "++idNELEHESS ++ "  " ++ show neleHess
   ,"#define "++idMEMSIZE++ "  " ++ show memSize
+  ,"#define "++idCONSTRAINTSIZE++ "  " ++ show constraintSize
   ]
 varDeclareTemplate :: [T.Text]
 varDeclareTemplate = map (T.pack . indent)
@@ -219,9 +225,11 @@ allocateTemplate objOffset = map (T.pack . indent)
   -- TODO allocate constraints g_L,g_U in allocateTemplate
   ]
 
-initializeTemplate :: [T.Text]
-initializeTemplate = undefined -- TODO finish initializeTemplate (break up into before/after createIpopt)
-                               -- initialize bounds and constraints before, initial point after
+initializeTemplate :: ValMaps -> MemMap -> ExpressionMap -> [T.Text]
+initializeTemplate valMaps memMap exprMap = map (T.pack . indent) $
+  ["/* Performing Initializion */"]
+  ++
+  generateAssignValueCodes valMaps memMap exprMap -- TODO load from file isn't configured?
 
 createIpoptTemplate :: Int -> Int -> [T.Text]
 createIpoptTemplate nele_jac nele_hess = map (T.pack . indent)
@@ -292,11 +300,12 @@ freeSolutionTemplate = map (T.pack . indent)
 
 -- | Template for objective function
 --
-evalfTemplate :: Int -> [String] -> [T.Text]
-evalfTemplate objOffset objEvalCode =
+evalfTemplate :: [(Int,Int)] -> Int -> Code -> Code -> [T.Text]
+evalfTemplate varsOffsetsSizes objOffset objEvalCode x2PtrMemCpyCode =
   let
     idNewX = "new_x"
     idObjVal = "obj_value"
+    idPtr = "ptr"
   in map (T.pack)
   ["Bool eval_f("++typeIdIndex++" "++idIndex++","
                  ++typeIdNumber++"* "++idVarX++","
@@ -313,27 +322,31 @@ evalfTemplate objOffset objEvalCode =
   ++
   padding
   ++
-  map (T.pack . indent) objEvalCode
+  -- TODO point x to SharedData and remove me (after fixing MemMap allocation)
+  map (T.pack . indent) x2PtrMemCpyCode
   ++
   padding
   ++
-  map (T.pack . indent)
-  ["*"++idObjVal++" = ptr["++show objOffset++"];"]
+  map (T.pack . indent) (["/* evaluate objective function here*/"] ++ objEvalCode)
+  ++
+  padding
+  ++
+  map (T.pack . indent) ["/* assign resulting objective function evaluation */"
+                        ,"*"++idObjVal++" = ptr["++show objOffset++"];"]
   ++
   [(T.pack . indent) "return TRUE;"
    ,T.pack "}"]
 
 -- | Template for objective function
 --
-evalGradfTemplate :: [(Int,Int)] -> [String] -> [T.Text]
-evalGradfTemplate objDiffOffsetsSizes objDiffEvalCode =
+evalGradfTemplate :: [(Int,Int)] -> Code -> Code -> [T.Text]
+evalGradfTemplate objDiffOffsetsSizes objDiffEvalCode x2PtrMemCpyCode =
   let
     idGradf = "grad_f"
     idPtr   = "ptr"
-    gradAssignsLoops = scoped $ ["int tmp = 0;"] ++ concatMap gradAssignLoop objDiffOffsetsSizes
-    gradAssignLoop (offset,sz) = forRange "i" sz [gradAssignCode "tmp" ("i + "++show offset)
-                                                 ,"tmp++;"]
-    gradAssignCode gradfIdx ptrIdx = idGradf++"["++gradfIdx++"]" <<- idPtr++"["++ptrIdx++"]"
+    valPtrOffsetPairs = zip objDiffOffsetsSizes (scanl (+) 0 $ map snd objDiffOffsetsSizes)
+    gradMemCpyCode = map (\((gOff,gSz),i) -> memcpyCode (idGradf,i) (idPtr,gOff) (typeIdNumber,gSz))
+                     valPtrOffsetPairs
   in map (T.pack)
   ["Bool eval_grad_f("++typeIdIndex++" "++idIndex++","
                       ++typeIdNumber++"* "++idVarX++","
@@ -349,13 +362,16 @@ evalGradfTemplate objDiffOffsetsSizes objDiffEvalCode =
   ++
   padding
   ++
-  map (T.pack . indent) objDiffEvalCode
+  -- TODO point x to SharedData and remove me (after fixing MemMap allocation)
+  map (T.pack .indent) x2PtrMemCpyCode
   ++
   padding
   ++
-  map (T.pack . indent )["/* populize grad_f here */"]-- TODO finish evalGradfTemplate'
+  map (T.pack . indent) (["/* evaluate gradient here */"] ++ objDiffEvalCode)
   ++
-  map (T.pack . indent) gradAssignsLoops
+  padding
+  ++
+  map (T.pack . indent ) (["/* copy values from SharedData */"] ++ gradMemCpyCode)
   ++
   padding
   ++
@@ -391,20 +407,24 @@ evalhTemplate =
   ,"{"
   ]
   ++
-  -- TODO finish evalhTemplate
-  -- (NOTE only necessary when turning off hessian_approximation option)
-  -- ++
+  -- NOTE when hessian_approximation option is on (as is current default) eval_h should be empty and return false
+  -- TODO implement option to turn hessian_approximation off and implement evaluation of hessian here
   [(T.pack . indent) "return FALSE; /* AddIpoptStrOption hessian_approximation set */" -- set to true if hessian_appromation is off
   ,T.pack "}"]
 
 -- | Template for constraint function
 --
-evalgTemplate :: [T.Text]
-evalgTemplate  =
+evalgTemplate :: [Int] -> [(Int,Int)] -> Code -> Code -> [T.Text]
+evalgTemplate scalarConsIDs scalarConsOffsetSizes scalarConsEvalCode x2PtrMemCpyCode =
   let
     idNewX = "new_x"
     idVarG = "g"
     idNumG = "m"
+    idPtr  = "ptr"
+    valPtrOffsetPairs = zip scalarConsOffsetSizes (scanl (+) 0 $ map snd scalarConsOffsetSizes)
+    -- FIXME ? correct size, should sSz == 1 for testExprAndConstraints?
+    constMemCpy = map (\((sOff,sSz),i) -> memcpyCode (idVarG,i) (idPtr,sOff) (typeIdNumber,sSz))
+                      valPtrOffsetPairs
   in map (T.pack)
   ["Bool eval_g("++typeIdIndex++" "++idIndex++","
                       ++typeIdNumber++"* "++idVarX++","
@@ -421,15 +441,27 @@ evalgTemplate  =
   ]
   ++
   padding
-  -- TODO finish evalgTemplate
+  ++
+  -- TODO point x to SharedData and remove me (after fixing MemMap allocation)
+  map (T.pack . indent) x2PtrMemCpyCode
+  ++
+  padding
+  ++
+  map (T.pack . indent) (["/* evaluate constraints */"] ++  scalarConsEvalCode)
+  ++
+  padding
+  ++
+  map (T.pack . indent) (["/* copy from SharedData */"] ++ constMemCpy)
+  ++
+  padding
   ++
   [(T.pack . indent) "return TRUE;"
    ,T.pack "}"]
 
 -- | Template for constraint function
 --
-evalJacTemplate :: [(Int,Int)] -> Code -> [T.Text]
-evalJacTemplate paritalScalarOffsetsSizes partialScalarConsEvalCode =
+evalJacTemplate :: [(Int,Int)] -> Code -> Code -> [T.Text]
+evalJacTemplate partialScalarOffsetsSizes partialScalarConsEvalCode x2PtrMemCpyCode =
   let
     idNewX = "new_x"
     idNumG = "m"
@@ -438,10 +470,9 @@ evalJacTemplate paritalScalarOffsetsSizes partialScalarConsEvalCode =
     idColSkip = "iCol"
     idValues = "values"
     idPtr = "ptr"
-    valuesAssignsLoops = scoped $ ["int tmp = 0;"] ++ concatMap valuesAssignLoop paritalScalarOffsetsSizes
-    valuesAssignLoop (offset,sz) = forRange "i" sz [valuesAssignCode "tmp" ("i + "++show offset)
-                                                 ,"tmp++;"]
-    valuesAssignCode gradfIdx ptrIdx = idValues++"["++gradfIdx++"]" <<- idPtr++"["++ptrIdx++"]"
+    valPtrOffsetPairs = zip partialScalarOffsetsSizes (scanl (+) 0 $ map snd partialScalarOffsetsSizes)
+    jacMemCpyCode = map (\((pOff,pSz),i) -> memcpyCode (idValues,i) (idPtr,pOff) (typeIdNumber,pSz))
+                    valPtrOffsetPairs
   in map (T.pack)
   ["Bool eval_jac_g("++typeIdIndex++" "++idIndex++","
                      ++typeIdNumber++"* "++idVarX++","
@@ -472,12 +503,18 @@ evalJacTemplate paritalScalarOffsetsSizes partialScalarConsEvalCode =
   ["}"
   ,"else {"]
   ++
-  map (T.pack . indent . indent) ["/* return the values of the jacbian of constraints */"]
-  --TODO implement jacbian evaluation of constraints in evalJacTemplate
+  -- TODO point x to SharedData and remove me (after fixing MemMap allocation)
+  map (T.pack . indent) (["/* copy objective vars to SharedData */"] ++ x2PtrMemCpyCode)
   ++
-  map (T.pack . indent) partialScalarConsEvalCode
+  padding
   ++
-  map (T.pack . indent) valuesAssignsLoops
+  map (T.pack . indent) ( ["/* eval values of the jacbian of constraints */"]
+                          ++ partialScalarConsEvalCode)
+  ++
+  padding
+  ++
+  map (T.pack . indent) ( ["/* copy values from SharedData */"]
+                          ++ jacMemCpyCode)
   ++
   [(T.pack . indent) "}"]
   ++
@@ -508,14 +545,14 @@ generateProblemIpopt valMaps problem@(Problem vars objId exprMap memMap mBoxCons
   in code `debug` (show vars)
 
 --expressionToIpopt :: Expression Scalar R -> [String] -> (Code,Problem)
-expressionToIpopt (expr@(Expression exprIdx exprMap),constraints) vars =
+expressionToIpopt valMaps (expr@(Expression exprIdx exprMap),constraints) vars =
   let
     setVars = Set.fromList vars
     problem@(Problem vrs objIdx exprMap memMap mBoxConstraints mScalarConstraints) =
       case constructProblem expr vars constraints of
         ProblemValid prob -> prob
         ProblemInvalid msg -> error $ "constructProblem Invalid: "++msg
-  in generateProblemIpopt undefined problem
+  in generateProblemIpopt valMaps problem
 
 testProblem :: Problem
 testProblem =
@@ -535,15 +572,10 @@ testExprAndConstraints =
     sCon = (const 2) * (x <.> y) .>= VScalar 1
   in (sumElements z,IPOPTConstraint [xCon,yCon,sCon])
 
-writeExprToIpopt file expr vars = TIO.writeFile file $ T.unlines $ expressionToIpopt expr vars
+testValMaps = Map.fromList [("x",(V2D $ Array.listArray ((0,0),(255,255)) $ repeat 0))
+                           ,("y",(V2D $ Array.listArray ((0,0),(255,255)) $ repeat 0))]
+
+writeExprToIpopt file valMaps expr vars = TIO.writeFile file $ T.unlines $ expressionToIpopt valMaps expr vars
 testFile = "testIpopt.c"
 
-runTest = writeExprToIpopt testFile testExprAndConstraints ["x","y"]
-
-x :: Maybe Int
-x = Nothing
-
-pullX :: HasCallStack => Int
-pullX = fromJust x
-
-something = error "Something happend"
+runTest = writeExprToIpopt testFile testValMaps testExprAndConstraints ["x","y"]
