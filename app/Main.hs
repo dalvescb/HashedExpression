ea22ca3d76a7ec3a26b658d379c629d950d12f1a
Nhan Thai (dandoh) <dainhan605@gmail.com>
2020-02-25 14:01:25 -0500

    [ Demo ] for Padma

--- app/Main.hs
+++ app/Main.hs
@@ -15,43 +15,40 @@ import HashedExpression.Interp
 import HashedExpression.Operation
 import qualified HashedExpression.Operation
 import HashedExpression.Prettify
-import Prelude hiding
-    ( (*)
-    , (+)
-    , (-)
-    , (/)
-    , (^)
-    , acos
-    , acosh
-    , asin
-    , asinh
-    , atan
-    , atanh
-    , const
-    , const
-    , cos
-    , cosh
-    , exp
-    , log
-    , negate
-    , product
-    , sin
-    , sinh
-    , sqrt
-    , sum
-    , tan
-    , tanh
-    )
-
 import Data.List (intercalate)
 import Data.Maybe (fromJust)
 import Data.STRef.Strict
 import Graphics.EasyPlot
+import HashedExpression.Internal.CollectDifferential
+import HashedExpression.Internal.ToC
+import HashedExpression.Internal.Utils
 import HashedExpression
 
 main :: IO ()
 main = do
-    print "hello world"
+    -- Encode and represent expressions
+    let x = variable1D @10 "x"
+    let y = variable1D @10 "y"
+    let z = variable1D @10 "z"
+    let exp = (x + y) <.> (x - y) + x <.> z
+    print exp
+    showExp exp
+    -- Normalizing expressions
+    let normalizedExp = normalize exp
+    showExp normalizedExp
+    -- Computing derivatives
+    let dExp = derivativeAllVars exp
+    showExp dExp
+    -- Collect differentials
+    let collectedDExp = collectDifferentials dExp
+    showExp collectedDExp
+    -- Convert to C
+    let valMap = fromList [("x", V1D $ listArray (0, 9) [1..10]), 
+         ("y", V1D $ listArray (0, 9) [1..10]),
+         ("z", V1D $ listArray (0, 9) [1..10])]
+    let cCode  = intercalate "\n" $ singleExpressionCProgram valMap normalizedExp
+    putStrLn cCode
+
 
 
 
